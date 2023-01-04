87b81c2022d7ce1f4246aa5769e2a918103aed79
aryansagar <aryansagar1996@gmail.com>
2023-01-04 16:11:18 +0530

    uber clone

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 
+
+import { Provider } from "react-redux"
+import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
+import { store } from "./store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-gesture-handler"
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { create } from "tailwind-react-native-classnames";
+
+
+/// 1) Redux 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <Provider store={store}>
+      <NavigationContainer>
+
+        <SafeAreaProvider>
+          <Stack.Navigator>
+           
+          <Stack.Screen 
+            name='HomeScreen' component={HomeScreen} options={{ headerShown: false}}/>
+          <Stack.Screen 
+            name='MapScreen' component={MapScreen} options={{ headerShown: false}}/>
+          </Stack.Navigator>
+
+        </SafeAreaProvider>
+
+      </NavigationContainer>
+
+
+    </Provider>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
